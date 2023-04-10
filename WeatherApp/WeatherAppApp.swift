68f522dc1f1d7bf8a1762ac4a472e7adc1edaa76
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-10 17:03:45 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by David Salmberg on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
